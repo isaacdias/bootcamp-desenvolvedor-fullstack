8f3c0f899dfd6a8c074e4f9ee4b4f78527abe630
Isaac Dias <isaacardias@gmail.com>
2020-08-10 20:06:36 -0300

    add exepmlos de classes com JavaScript

---
+++ modulo03-react/classes/js/index.js
@@ -0,0 +1,40 @@
+// Criando uma classe com JS
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} falando...`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, type) {
+    super(name);
+
+    this.type = type;
+  }
+
+  speak() {
+    console.log(`${this.name} (${this.type}) latindo...`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, type) {
+    super(name);
+
+    this.type = type;
+  }
+
+  speak() {
+    console.log(`${this.name} (${this.type}) miando...`);
+  }
+}
+const animal = new Animal('Totó');
+const dog = new Dog('Jack', 'Poodle');
+const cat = new Cat('Marvin', 'Frajola');
+animal.speak();
+dog.speak();
+cat.speak();
