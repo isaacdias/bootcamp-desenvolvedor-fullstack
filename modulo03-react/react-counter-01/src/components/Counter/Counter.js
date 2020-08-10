2599d26bf85b3fbbea3ad843fa2ac29be6d6dde1
Isaac Dias <isaacardias@gmail.com>
2020-08-10 20:53:14 -0300

    class components parte 1

---
+++ modulo03-react/react-counter-01/src/components/Counter/Counter.js
@@ -0,0 +1,8 @@
+// rcc tab é um atalho para criar class components
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+  render() {
+    return <div>Counter</div>;
+  }
+}
