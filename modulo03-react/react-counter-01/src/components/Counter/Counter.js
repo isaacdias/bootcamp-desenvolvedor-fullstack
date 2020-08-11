485cbd1e5ac0080cc740619f5031dbff8305bc97
Isaac Dias <isaacardias@gmail.com>
2020-08-11 09:02:08 -0300

    class components parte 2

--- modulo03-react/react-counter-01/src/components/Counter/Counter.js
+++ modulo03-react/react-counter-01/src/components/Counter/Counter.js
@@ -1,8 +1,34 @@
 // rcc tab é um atalho para criar class components
 import React, { Component } from 'react';
+import css from './counter.module.css';
 
 export default class Counter extends Component {
+  constructor() {
+    super();
+
+    this.currentCounter = 2;
+  }
+
+  handleClick = () => {
+    console.log('click');
+    this.currentCounter--;
+    this.render();
+  };
+
   render() {
-    return <div>Counter</div>;
+    return (
+      <div className={css.counterContainer}>
+        <button
+          onClick={this.handleClick}
+          className="waves-effect waves-light btn red darken-4"
+        >
+          -
+        </button>
+        <span className={css.counterValue}>{this.currentCounter}</span>
+        <button className="waves-effect waves-light btn green darken-4">
+          +
+        </button>
+      </div>
+    );
   }
 }
